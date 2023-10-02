257f9e5fac315b4fadb7257d84a65de6de003bb0
jay parmar <145897830+codecraftuniverse@users.noreply.github.com>
2023-10-02 17:58:05 +0530

    Delete code directory

--- code/js/main.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector("input");
-const body = document.querySelector("body");
-
-const toggleThemeMode = () => {
-  body.classList.toggle("dark");
-};
-
-input.onchange = toggleThemeMode;
